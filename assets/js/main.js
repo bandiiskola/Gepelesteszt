32806842fd32fef77d62a6cb0ad08bad3ca32444
bandiiskola <nandor.bere@gmail.com>
2023-10-04 10:20:15 +0200

    Jo

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+var szovegek = ["lorem ipsum dolor sit amet",
+    "Sanyi és Peti mindig ötös",
+    "Tomi mosóport vettem tegnap",
+    "Nincs kettő négy nélkül",
+    "Ki mint veti ágyát úgy alussza álmát"
+]
